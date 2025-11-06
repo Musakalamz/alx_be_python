5b2e05d6f269db1776ea765d9e3e278aa6c3be07
Musakalamz <ogunsolumusa@gmail.com>
2025-11-06 16:55:48 +0100

    Persona; Finance Calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+#  prompt user for monthly input and expenses and calculate yearly income incl tax.
+
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${monthly_savings:.0f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.0f}.")
