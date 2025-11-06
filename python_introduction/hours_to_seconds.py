d3f05cc4a0c09a082456f99b8cfd30f8a7484684
Musakalamz <ogunsolumusa@gmail.com>
2025-11-06 15:35:30 +0100

    Converts Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# Calculate and convert hours to seconds
+
+hours = 2
+mins_in_one_hour = 60
+secs_in_one_min = 60
+
+seconds = hours * mins_in_one_hour * secs_in_one_min
+
+print(hours, "hour(s) is", seconds, "seconds.")
